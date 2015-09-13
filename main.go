4dc253ce827dc85e54cbc0968963c29c1b17c0eb
konjoot <konjoot@gmail.com>
2015-09-13 13:08:32 +0300

    initial templating functionality

--- main.go
+++ main.go
@@ -1,5 +1,40 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"os"
 
+	t "html/template"
+)
+
+var graph *t.Template
+
+func init() {
+	defer func() { ExitIf(recover()) }()
+
+	// some template caching there
+	graph = t.Must(t.ParseFiles("graph.thtml"))
+}
+
+func main() {
+	defer func() { ExitIf(recover()) }()
+
+	var (
+		file *os.File
+		err  error
+	)
+
+	file, err = os.Create("graph.html")
+	defer file.Close()
+	ExitIf(err)
+
+	err = graph.Execute(file, "gopher")
+	ExitIf(err)
+}
+
+func ExitIf(err interface{}) {
+	if err != nil {
+		fmt.Println("Error: ", err)
+		os.Exit(1)
+	}
 }
