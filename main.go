e6def68e88f2c4d64bfb15fb37f3069ba6d5da50
Maksimov Sergey <konjoot@gmail.com>
2015-09-15 20:21:22 +0300

    inject json-data into html

--- main.go
+++ main.go
@@ -9,25 +9,39 @@ import (
 
 var graph *t.Template
 
+type data struct {
+	Items []item
+}
+
+type item map[string]string
+
 func init() {
-	defer func() { ExitIf(recover()) }()
+	defer func() { exitIf(recover()) }()
 
 	// some template caching there
 	graph = t.Must(t.ParseFiles("graph.thtml"))
 }
 
 func main() {
-	defer func() { ExitIf(recover()) }()
+	defer func() { exitIf(recover()) }()
 
 	file, err := os.Create("graph.html")
 	defer file.Close()
-	ExitIf(err)
+	exitIf(err)
 
-	err = graph.Execute(file, "gopher")
-	ExitIf(err)
+	// data := parse("path/to/file")
+
+	dt := &data{Items: []item{
+		item{"x": "1,2", "y": "0"},
+		item{"x": "2,3", "y": "1"},
+		item{"x": "3,4", "y": "3"},
+		item{"x": "5,5", "y": "4"}}}
+
+	err = graph.Execute(file, dt)
+	exitIf(err)
 }
 
-func ExitIf(err interface{}) {
+func exitIf(err interface{}) {
 	if err != nil {
 		fmt.Println("Error: ", err)
 		os.Exit(1)
