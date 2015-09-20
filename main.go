e5d140fb6abeb08fea56214e08d587295ab36a81
Maksimov Sergey <konjoot@gmail.com>
2015-09-20 20:32:31 +0300

    parse file into the array of maps

--- main.go
+++ main.go
@@ -1,19 +1,29 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"io"
 	"os"
+	"strings"
 
 	t "html/template"
 )
 
+const (
+	TAB = "\t"
+	EOL = '\n'
+)
+
 var graph *t.Template
 
-type data struct {
-	Items []item
-}
+type (
+	data struct {
+		Items []item
+	}
 
-type item map[string]string
+	item map[string]string
+)
 
 func init() {
 	defer func() { exitIf(recover()) }()
@@ -29,16 +39,71 @@ func main() {
 	defer file.Close()
 	exitIf(err)
 
-	// data := parse("path/to/file")
+	fmt.Printf("%#v", newDataFromFile("./data.txt"))
+	// err = graph.Execute(file, &data{newDataFromFile("./data.txt")})
+	// exitIf(err)
+}
 
-	dt := &data{Items: []item{
-		item{"x": "1,2", "y": "0"},
-		item{"x": "2,3", "y": "1"},
-		item{"x": "3,4", "y": "3"},
-		item{"x": "5,5", "y": "4"}}}
-
-	err = graph.Execute(file, dt)
+func newDataFromFile(path string) []item {
+	file, err := os.Open(path)
 	exitIf(err)
+
+	defer file.Close()
+
+	fSize, err := countLinesIn(file)
+	exitIf(err)
+
+	_, err = file.Seek(0, 0)
+	exitIf(err)
+
+	result := make([]item, fSize-1)
+
+	// prepare scanner for file
+	line := bufio.NewScanner(file)
+
+	// header initializtion
+	line.Scan()
+	header := strings.Split(line.Text(), TAB)
+
+	var n int // line number counter
+	// scan file and fill the result
+	for line.Scan() {
+		record := make(item)
+
+		// map line to item where TAB("\t") is separator
+		for i, word := range strings.Split(line.Text(), TAB) {
+			record[header[i]] = word
+		}
+
+		result[n] = record // push to result
+		n++                // increment line number
+	}
+
+	return result
+}
+
+func countLinesIn(r io.Reader) (n int, err error) {
+	var count int
+
+	buf := make([]byte, 8<<10) // 8Kb
+
+	for {
+		count, err = r.Read(buf)
+
+		if err == io.EOF {
+			return n, nil
+		}
+
+		if err != nil {
+			return
+		}
+
+		for _, b := range buf[:count] {
+			if b == EOL {
+				n++
+			}
+		}
+	}
 }
 
 func exitIf(err interface{}) {
